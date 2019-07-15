70c0706ef55314f253b6ff52262c60230fa9422d
Phil Lucks <philip.lucks@snapengage.com>
2019-07-14 18:44:39 -0600

    consistent

--- src/features/SearchWeather/Search/index.js
+++ src/features/SearchWeather/Search/index.js
@@ -1,19 +1,9 @@
 import React from "react";
-import styled from "styled-components";
 
 import Input from "../../../components/input";
 import Button from "../../../components/button";
 
-const StyledWrapperSearch = styled.div`
-  height: 32px;
-  display: flex;
-  align-items: center;
-  flex-flow: row wrap;
-
-  input {
-    flex: 1 0 auto;
-  }
-`;
+import { StyledWrapperSearch } from "./SearchWrapper.css";
 
 const Search = props => {
   const { submit, handleChange, handleClick } = props;
